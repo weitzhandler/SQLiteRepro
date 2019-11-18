b9f286d43ef942b5f7c04f193131a786652fd8dc
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2019-11-18 08:34:22 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      using var context = new Context();
+      context.Database.EnsureCreated();
+    }
+  }
+
+  class Context : DbContext
+  {
+    public DbSet<Model> Models { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      var model = modelBuilder.Entity<Model>();
+      model.Property(m => m.Id).ValueGeneratedOnAdd();
+      model.HasData(new Model { Id = 1, Value = "Lorem ipsum dolor sit amet" });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Data Source=App.db");
+    }
+  }
+
+  class Model
+  {
+    public int Id { get; set; }
+    public string Value { get; set; }
+  }
+
+
+}
